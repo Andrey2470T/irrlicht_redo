fbf88f51385b4f00a50f395187d6e5269d9f183d
Andrey2470T <andreyt2203@gmail.com>
2024-12-21 13:58:21 +0300

    Auto create the position and color attributes and optionally the normal and uv in the vertex type

--- src/Render/VertexTypeDescriptor.h
+++ src/Render/VertexTypeDescriptor.h
@@ -5,9 +5,10 @@
 namespace render
 {
 
-// Descriptor of the vertex attribute
+// Descriptor of the vertex attribute.
 struct VertexAttribute
 {
+	// 'Regular' means the non-normalized attribute.
 	enum class DataFormat
 	{
 		Regular,
@@ -15,7 +16,7 @@ struct VertexAttribute
 		Integral,
 	};
 	std::string Name;
-	u32 ComponentCount;
+	u8 ComponentCount;
 	BasicType ComponentType;
 	DataFormat Format;
 
@@ -26,19 +27,54 @@ struct VertexAttribute
 	}
 };
 
-// Descriptor of the vertex type
-struct VertexTypeDescriptor
+// Descriptor of the vertex type.
+// Always contains at least 2 attributes (float 3-component position and uint8 color).
+// Also optionally create the float 3-component normal and float 2 or 3-component uv.
+// These 4 attrubutes are hardcoded for simplicity of the transformations over the meshes
+// and passed in the shader always in the same look.
+class VertexTypeDescriptor
 {
+public:
+	// Name of the vertex type.
 	std::string Name;
+	// List of all attributes.
 	std::vector<VertexAttribute> Attributes;
 
+	// Constructor with optional passing the normal and uv.
+	VertexTypeDescriptor(bool init_normal=false, bool init_uv=false, u8 uv_count=0)
+		: Name("")
+	{
+		initAttributes(init_normal, init_uv, uv_count);
+	}
+	// Constructor with passing the vertex type name, custom attributes and optionally the normal and uv.
+	VertexTypeDescriptor(const std::string &name, const std::vector<VertexAttribute> &attributes,
+		bool init_normal=false, bool init_uv=false, u8 uv_count=0)
+		:Name(name)
+	{
+		initAttributes(init_normal, init_uv, uv_count);
+		
+		for (const auto &attr : attributes)
+			Attributes.push_back(attr);
+	}
 	bool operator==(const VertexType &other)
 	{
 		return Attributes == other.Attributes;
 	}
+private:
+	void initAttributes(bool init_normal=false, bool init_uv=false, u8 uv_count=0)
+	{
+		Attributes.push_back({"Position", 3, BasicType::FLOAT, VertexAttribute::DataFormat::Regular});
+		Attributes.push_back({"Color", 4, BasicType::UINT8, VertexAttribute::DataFormat::Normalized});
+		
+		if (init_normal)
+			Attributes.push_back({"Normal", 3, BasicType::FLOAT, VertexAttribute::DataFormat::Regular});
+		
+		if (init_uv)
+			Attributes.push_back({"UV", uv_count, BasicType::FLOAT, VertexAttribute::DataFormat::Regulard});
+	}
 };
 
-// Returns size of the vertex type in bytes
+// Returns size of the vertex type in bytes.
 extern size_t sizeOfVertexType(const VertexTypeDescriptor &vtype)
 {
 	size_t size;
@@ -49,23 +85,13 @@ extern size_t sizeOfVertexType(const VertexTypeDescriptor &vtype)
 }
 
 
-// Default 3D vertex type (position, normal, color and uv)
-enum class DefaultVTypeAttrs
-{
-	POSITION = 0,
-	NORMAL,
-	COLOR,
-	UV
-};
-
-static const VertexTypeDescriptor DefaultVType = {
+// Default 3D vertex type (position, color, normal and uv).
+extern const VertexTypeDescriptor DefaultVType{
 	"3D_Default",
-	{
-		{"Position", 3, BasicType::FLOAT, VertexAttribute::DataFormat::Regular},
-		{"Normal", 3, BasicType::FLOAT, VertexAttribute::DataFormat::Regular},
-		{"Color", 4, BasicType::UINT8, VertexAttribute::DataFormat::Normalized},
-		{"UV", 2, BasicType::FLOAT, VertexAttribute::DataFormat::Regulard},
-	}
+	{},
+	true,
+	true,
+	2
 };
 
 }
