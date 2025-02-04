9368c1350316974df851eeb00a5c7722d189624a
Andrey2470T <andreyt2203@gmail.com>
2025-02-04 14:20:15 +0300

    Moved some color code to cpp

---
+++ src/Image/Color.cpp
@@ -0,0 +1,103 @@
+#include "Color.h"
+
+namespace img
+{
+
+inline void ColorHSL::fromRGBA(const ColorRGBA<f32> &color)
+{
+    const f32 maxVal = utils::max3<f32>(color.R(), color.G(), color.B());
+    const f32 minVal = utils::min3<f32>(color.R(), color.G(), color.B());
+
+	L = (maxVal + minVal) * 50;
+
+    if (utils::equals(maxVal, minVal)) {
+		H = 0.0f;
+		S = 0.0f;
+		return;
+	}
+
+	const f32 delta = maxVal - minVal;
+	if (L <= 50) {
+		S = (delta) / (maxVal + minVal);
+	} else {
+		S = (delta) / (2 - maxVal - minVal);
+	}
+	S *= 100;
+
+    if (utils::equals(maxVal, color.R()))
+        H = (color.G() - color.B()) / delta;
+    else if (utils::equals(maxVal, color.G()))
+        H = 2 + ((color.B() - color.R()) / delta);
+	else // blue is max
+        H = 4 + ((color.R() - color.G()) / delta);
+
+	H *= 60.0f;
+	while (H < 0.0f)
+		H += 360;
+}
+
+inline void ColorHSL::toRGBA(ColorRGBA<f32> &color) const
+{
+	const f32 l = L / 100;
+    if (utils::equals(S, 0.0f)) { // grey
+        color.R(l);
+        color.G(l);
+        color.B(l);
+		return;
+	}
+
+	f32 rm2;
+
+	if (L <= 50) {
+		rm2 = l + l * (S / 100);
+	} else {
+		rm2 = l + (1 - l) * (S / 100);
+	}
+
+	const f32 rm1 = 2.0f * l - rm2;
+
+	const f32 h = H / 360.0f;
+    color.R(toRGBA1(rm1, rm2, h + 1.0f / 3.0f));
+    color.G(toRGBA1(rm1, rm2, h));
+    color.B(toRGBA1(rm1, rm2, h - 1.0f / 3.0f));
+}
+
+// algorithm from Foley/Van-Dam
+inline f32 ColorHSL::toRGBA1(f32 rm1, f32 rm2, f32 rh) const
+{
+	if (rh < 0)
+		rh += 1;
+	if (rh > 1)
+		rh -= 1;
+
+	if (rh < 1.f / 6.f)
+		rm1 = rm1 + (rm2 - rm1) * rh * 6.f;
+	else if (rh < 0.5f)
+		rm1 = rm2;
+	else if (rh < 2.f / 3.f)
+		rm1 = rm1 + (rm2 - rm1) * ((2.f / 3.f) - rh) * 6.f;
+
+	return rm1;
+}
+
+color8 getColor8(ByteArray *arr, u32 n)
+{
+	color8 c(PF_RGBA8);
+
+	c.R(arr->getUInt8(n));
+	c.G(arr->getUInt8(n+1));
+	c.B(arr->getUInt8(n+2));
+	c.A(arr->getUInt8(n+3));
+
+	return c;
+}
+
+void setColor8(ByteArray *arr, color8 c, std::optional<u32> n)
+{
+	arr->setUInt8(c.R(), n);
+	arr->setUInt8(c.G(), n ? n.value()+1 : n);
+	arr->setUInt8(c.B(), n ? n.value()+2 : n);
+	arr->setUInt8(c.A(), n ? n.value()+3 : n);
+}
+
+}
--- src/Image/Color.h
+++ src/Image/Color.h
@@ -3,6 +3,7 @@
 #include "Utils/ByteArray.h"
 #include "PixelFormats.h"
 #include "Utils/MathFuncs.h"
+#include <optional>
 
 namespace img {
 
@@ -323,6 +324,9 @@ private:
 	}
 };
 
+typedef ColorRGBA<u8> color8;
+typedef ColorRGBA<f32> colorf;
+
 //! Class representing a color in HSL format
 /** The color values for hue, saturation, luminance
 are stored in 32bit floating point variables. Hue is in range [0,360],
@@ -343,111 +347,14 @@ public:
 	ColorHSL(f32 h, f32 s, f32 l)
 		: H(h), S(s), L(l) {}
 
-	void fromRGBA(const ColorRGBA<f32> &color);
-	void toRGBA(ColorRGBA<f32> &color) const;
+	inline void fromRGBA(const ColorRGBA<f32> &color);
+	inline void toRGBA(ColorRGBA<f32> &color) const;
 private:
 	inline f32 toRGBA1(f32 rm1, f32 rm2, f32 rh) const;
 };
 
-inline void ColorHSL::fromRGBA(const ColorRGBA<f32> &color)
-{
-    const f32 maxVal = utils::max3<f32>(color.R(), color.G(), color.B());
-    const f32 minVal = utils::min3<f32>(color.R(), color.G(), color.B());
-
-	L = (maxVal + minVal) * 50;
-
-    if (utils::equals(maxVal, minVal)) {
-		H = 0.0f;
-		S = 0.0f;
-		return;
-	}
-
-	const f32 delta = maxVal - minVal;
-	if (L <= 50) {
-		S = (delta) / (maxVal + minVal);
-	} else {
-		S = (delta) / (2 - maxVal - minVal);
-	}
-	S *= 100;
-
-    if (utils::equals(maxVal, color.R()))
-        H = (color.G() - color.B()) / delta;
-    else if (utils::equals(maxVal, color.G()))
-        H = 2 + ((color.B() - color.R()) / delta);
-	else // blue is max
-        H = 4 + ((color.R() - color.G()) / delta);
-
-	H *= 60.0f;
-	while (H < 0.0f)
-		H += 360;
-}
-
-inline void ColorHSL::toRGBA(ColorRGBA<f32> &color) const
-{
-	const f32 l = L / 100;
-    if (utils::equals(S, 0.0f)) { // grey
-        color.R(l);
-        color.G(l);
-        color.B(l);
-		return;
-	}
-
-	f32 rm2;
-
-	if (L <= 50) {
-		rm2 = l + l * (S / 100);
-	} else {
-		rm2 = l + (1 - l) * (S / 100);
-	}
-
-	const f32 rm1 = 2.0f * l - rm2;
-
-	const f32 h = H / 360.0f;
-    color.R(toRGBA1(rm1, rm2, h + 1.0f / 3.0f));
-    color.G(toRGBA1(rm1, rm2, h));
-    color.B(toRGBA1(rm1, rm2, h - 1.0f / 3.0f));
-}
-
-// algorithm from Foley/Van-Dam
-inline f32 ColorHSL::toRGBA1(f32 rm1, f32 rm2, f32 rh) const
-{
-	if (rh < 0)
-		rh += 1;
-	if (rh > 1)
-		rh -= 1;
-
-	if (rh < 1.f / 6.f)
-		rm1 = rm1 + (rm2 - rm1) * rh * 6.f;
-	else if (rh < 0.5f)
-		rm1 = rm2;
-	else if (rh < 2.f / 3.f)
-		rm1 = rm1 + (rm2 - rm1) * ((2.f / 3.f) - rh) * 6.f;
-
-	return rm1;
-}
-
-typedef ColorRGBA<u8> color8;
-typedef ColorRGBA<f32> colorf;
-
-color8 getColor8(ByteArray *arr, u32 n)
-{
-	color8 c(PF_RGBA8);
-
-	c.R(arr->getUInt8(n));
-	c.G(arr->getUInt8(n+1));
-	c.B(arr->getUInt8(n+2));
-	c.A(arr->getUInt8(n+3));
-
-	return c;
-}
-
-void setColor8(ByteArray *arr, color8 c, u32 n)
-{
-	arr->setUInt8(c.R(), n);
-	arr->setUInt8(c.G(), n+1);
-	arr->setUInt8(c.B(), n+2);
-	arr->setUInt8(c.A(), n+3);
-}
+color8 getColor8(ByteArray *arr, u32 n);
+void setColor8(ByteArray *arr, color8 c, std::optional<u32> n);
 
 }
 
