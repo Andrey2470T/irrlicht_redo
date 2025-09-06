e962c34fd3aaadd84e30bc53a8a996980a90fd4d
Andrey2470T <andreyt2203@gmail.com>
2025-09-06 20:00:49 +0300

    TypeConverter: toV2u and toV3u

--- src/Utils/TypeConverter.h
+++ src/Utils/TypeConverter.h
@@ -13,6 +13,11 @@ namespace utils
 		return v2i(v.X, v.Y);
 	}
 
+    v2u toV2u(const v2i &v)
+    {
+        return v2u(v.X, v.Y);
+    }
+
 	v3f toV3f(const v3i &v)
 	{
 		return v3f(v.X, v.Y, v.Z);
@@ -22,6 +27,11 @@ namespace utils
 	{
 		return v3i(v.X, v.Y, v.Z);
 	}
+
+    v3u toV3u(const v3i &v)
+    {
+        return v3u(v.X, v.Y, v.Z);
+    }
 
 	rectf toRectf(const recti &r)
 	{
