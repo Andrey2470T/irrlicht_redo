bdf1190a5964ac108697b029b2c588a89c12d73c
Andrey2470T <andreyt2203@gmail.com>
2025-09-28 12:56:46 +0300

    Use C++17 filesystem instead of the experimental one, check for APPLE for using another GL include path

--- src/FilesystemVersions.h
+++ src/FilesystemVersions.h
@@ -1,11 +1,3 @@
-#ifdef OSX
 #include <filesystem>
 
 namespace fs = std::filesystem;
-
-#else
-#include <experimental/filesystem>
-
-namespace fs = std::experimental::filesystem;
-
-#endif
--- src/OpenGLIncludes.h
+++ src/OpenGLIncludes.h
@@ -1,5 +1,11 @@
 #pragma once
 
 #include <GL/glew.h>
+
+#ifdef OSX
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
+
 #include <SDL_opengl.h>
